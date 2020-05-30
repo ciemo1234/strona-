922058e8d64476b881f424843ab2d6fa4638b69a
Przemysław Barć <w63625@student.wsiz.rzeszow.pl>
2020-05-30 11:29:34 +0200

    wprowadzenie podstron

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,13 @@
 import { RouterModule } from '@angular/router';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { AvailableCarsComponent } from './available-cars/available-cars.component';
 import { MyReservationsComponent } from './my-reservations/my-reservations.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
@@ -19,7 +21,9 @@ import { MyReservationsComponent } from './my-reservations/my-reservations.compo
     RouterModule.forRoot([
         {path: 'myreservations',component: MyReservationsComponent },
         {path: 'availablecars', component: AvailableCarsComponent },
-    ])
+    ]),
+    NgbModule,
+    FormsModule, ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
